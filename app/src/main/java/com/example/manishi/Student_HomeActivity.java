f5adbe0653c8365b4326e586ec10852090e81e53
ganasampath <suryasampath212@gmail.com>
2022-09-28 10:47:14 +0530

    Home

---
+++ app/src/main/java/com/example/manishi/Student_HomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.manishi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Student_HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_home);
+    }
+}
